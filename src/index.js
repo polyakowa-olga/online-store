173580ecd34034b99c8d8562ebdaa15b562d964b
polyakowa-olga <75207400+polyakowa-olga@users.noreply.github.com>
2022-12-20 18:19:25 +0400

    Html basic components (#2)
    * fix: fix eslint error
    
    * feat: add header and footer to html.page
    
    * fix: change base color

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import "./main.scss";
-// import example from "./images/logo.svg";
-// import example2 from "./images/image.png";
+
+import './main.scss';
+
+import github_icon_128848 from "./images/github_icon_128848.svg";
+import rs_school_js from "./images/rs_school_js.svg";
+import cart from "./images/images.png";
+
+
 import { test } from "./exampl";
 import { component } from "./components/item/item";
+
 
 // function component(text) {
 //   const element = document.createElement("h1");
@@ -13,6 +19,9 @@ import { component } from "./components/item/item";
 test("hello, TS файл работает");
 component();
 
+
+
 // for (let i = 0; i <= data.products.length; i++) {
 //   document.body.appendChild(component(data.products[i].title));
 // }
+
