0fa6c49415d22972b5af772ce026441f05d507fc
Olga Polyakowa <polyakowa.o2011@yandex.ru>
2022-12-21 18:07:07 +0400

    refactor: change lines in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import "./main.scss";
+import './main.scss';
 
 import github_icon_128848 from "./assets/images/github_icon_128848.svg";
 import rs_school_js from "./assets/images/rs_school_js.svg";
 import cart from "./assets/images/images.png";
 import logo from "./assets/images/logo.jpg";
 
-import { showProducts } from "./components/main/products";
-showProducts();
 import { test } from "./exampl";
 import { component } from "./components/item/item";
 
@@ -22,10 +20,10 @@ console.log(filter(data.products, "category", "laptops"));
 
 test("hello, TS файл работает");
 component();
-
+import { showProducts } from "./components/main/products";
+showProducts();
 
 
 // for (let i = 0; i <= data.products.length; i++) {
 //   document.body.appendChild(component(data.products[i].title));
 // }
-
