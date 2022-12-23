9f28292b43767237ac956e4a22ca9ff3daaee939
Olga Polyakowa <polyakowa.o2011@yandex.ru>
2022-12-23 20:59:43 +0400

    fix: clean index.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,13 @@
-import './main.scss';
+import "./main.scss";
 
 import github_icon_128848 from "./assets/images/github_icon_128848.svg";
 import rs_school_js from "./assets/images/rs_school_js.svg";
 import cart from "./assets/images/images.png";
 import logo from "./assets/images/logo.jpg";
 
-import { component } from "./components/item/item";
 import { showProducts } from "./components/main/products";
-
+showProducts();
 
 import data from "./assets/data.json";
 import { drawFilterSection } from "./components/filter.ts";
-
-import data from "./assets/data.json";
-import { filter } from "./components/filter.ts";
-console.log(filter(data.products, "category", "laptops"));
-
-// function component(text) {
-//   const element = document.createElement("h1");
-//   element.textContent = text;
-//   return element;
-// }
-
-component();
-
-showProducts();
-
-
-// for (let i = 0; i <= data.products.length; i++) {
-//   document.body.appendChild(component(data.products[i].title));
-// }
-
-
-
-////// filter ///////////////////////////////////////////////////////////////////////////////////////////////////
-
 drawFilterSection(data.products);
-///// filter END //////////////////////////////////////////////////////////////////////////////////////////////////////
